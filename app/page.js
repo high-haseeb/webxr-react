2dfbe51ca39d3c7518b444e24c67af28c5d0f544
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-01-04 18:50:21 +0500

    made some reflection chaanges

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Model from "@/componennts/Model";
 import { PresentationControls, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Html } from "@react-three/drei";
-import { Suspense, useEffect } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCamera,
@@ -45,24 +45,73 @@ const TopBar = () => {
 };
 
 const BottomBar = ({ modelPath }) => {
-  useEffect(() => {
-    document
-      .getElementById("arLink")
-      .addEventListener("click", function (event) {
-        event.preventDefault(); // Prevent the default behavior (downloading the file)
-      });
-  });
-  return (
-    <div className="absolute bottom-4  flex items-center justify-evenly w-1/4 fa-2x ">
-      <FontAwesomeIcon icon={faCube} className="" />
-      <FontAwesomeIcon icon={faShare} />
-      <FontAwesomeIcon icon={faExpand} />
+  // Function to get the device type
+  const getDeviceType = () => {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/android/i.test(userAgent)) {
+      return "android";
+    } else if (/iPhone/i.test(userAgent) || /iPad/i.test(userAgent)) {
+      return "iphone";
+    } else {
+      return "desktop";
+    }
+  };
 
-      <a href="intent://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf&mode=ar_preferred#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">
-        <a rel="ar" id="arLink" href={modelPath}>
-          <FontAwesomeIcon icon={faVrCardboard} />
-        </a>
+  // Function to generate the appropriate href based on the device type
+  const generateHref = () => {
+    const deviceType = getDeviceType();
+    if (deviceType === "android") {
+      return `intent://arvr.google.com/scene-viewer/1.0?file=${modelPath}&mode=ar_preferred#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;`;
+    } else if (deviceType === "iphone") {
+      return modelPath;
+    } else {
+      return "#desktopHref";
+    }
+  };
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <div className="absolute bottom-4  flex items-center justify-evenly w-1/4 fa-2x pointer-events-auto cursor-pointer">
+      <FontAwesomeIcon
+        icon={faCube}
+        onClick={() => setOpen((state) => !state)}
+        title="Standard Veiws"
+      />
+      <FontAwesomeIcon icon={faShare} title="Share" />
+      <FontAwesomeIcon icon={faExpand} title="Full Screen" />
+      <StandardVeiwMenu isOpen={isOpen} onClose={() => setOpen(state => !state)} />
+      <a rel="ar" id="arLink" href={generateHref()} title="Open AR">
+        <FontAwesomeIcon icon={faVrCardboard} />
       </a>
+    </div>
+  );
+};
+
+const StandardVeiwMenu = ({ isOpen, onClose }) => {
+  const modalRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
+  return (
+    <div
+      className={`absolute w-16 h-16 bg-zinc-600 ${isOpen ? "block" : "hidden"}`}
+    >
+      <div ref={modalRef} className="modal-content">
+        <h1>Hello world</h1>
+      </div>
     </div>
   );
 };
